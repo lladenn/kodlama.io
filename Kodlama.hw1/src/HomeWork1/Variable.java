f489ff238163300eb7657440d9de6ee793d3eb9d
LADEN <LADEN@LAPTOP-UNIJM9U4>
2022-09-23 00:44:05 +0300

    variable

---
+++ Kodlama.hw1/src/HomeWork1/Variable.java
@@ -0,0 +1,16 @@
+package HomeWork1;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		
+		int ogrenciSayısı =12 ;
+		String mesaj ="Öğrenci sayısı : ";
+		System.out.println(mesaj + ogrenciSayısı);
+		System.out.println(mesaj + ogrenciSayısı);
+		System.out.println("Öğrenci sayım : "+ ogrenciSayısı);
+		System.out.println("Öğrenci sayım : "+ ogrenciSayısı);
+
+	}
+
+}
