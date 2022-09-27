d65701033dcaf0f712221541b20042484520feed
LADEN <LADEN@LAPTOP-UNIJM9U4>
2022-09-27 21:39:28 +0300

    hw2

--- Kodlama.hw2/src/HomeWork2/DortIslem.java
+++ Kodlama.hw2/src/HomeWork2/DortIslem.java
@@ -16,6 +16,9 @@ public class DortIslem {
 	public int Bol ( int sayi1, int sayi2) {
 		return sayi1/sayi2;
 	}
+	public int Topla(int sayi1, int sayi2, int sayi3) {
+		return sayi1 + sayi2 + sayi3;
+	}
 
 }
 
---
+++ Kodlama.hw2/src/HomeWork2/methodOverloading.java
@@ -0,0 +1,11 @@
+package HomeWork2;
+
+public class methodOverloading {
+
+	public static void main(String[] args) {
+		DortIslem dortIslem= new DortIslem();
+		System.out.println(dortIslem.Topla(2, 3,5));
+
+	}
+
+}
